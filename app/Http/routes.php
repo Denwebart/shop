3529601010cfc4596c1827f0c71a8e575a44ca8c
Annywebart <annywebart@yandex.ua>
2016-04-26 18:09:32 +0300

    Исправление роутов для админки.

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -3,4 +3,7 @@
 Route::auth();
 
 Route::get('/', 'SiteController@index');
+
+Route::get('admin', ['as' => 'admin.index', 'uses' => 'AdminController@index']);
+
 Route::get('{products}', ['as' => 'product.productInfo', 'uses' => 'ProductController@productInfo']);
