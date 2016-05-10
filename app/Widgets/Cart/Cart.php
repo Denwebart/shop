edac93898b141b28b0948a8a56b631674d4e019e
Denwebart <denwebart@yandex.ua>
2016-05-10 18:00:08 +0300

    Добавление товаров в корзину (добавление в сессию).

--- app/Widgets/Cart/Cart.php
+++ app/Widgets/Cart/Cart.php
@@ -8,7 +8,6 @@
 
 namespace App\Widgets\Cart;
 
-use App\Models\Page;
 use Illuminate\Routing\Controller as BaseController;
 use App\Models\Product;
 use Illuminate\Http\Request;
@@ -17,6 +16,7 @@ class Cart extends BaseController
 {
 	public function show()
 	{
+		//доделать вывод товаров корзины
 		$products = Product::with(['category', 'category.parent'])->get();
 
 		return view('widget.cart::cart', compact('products'));
@@ -27,10 +27,11 @@ class Cart extends BaseController
 		if($request->ajax()) {
 
 			$product = Product::find($request->get('id'));
-			if($product) {
+			if(is_object($product)) {
 				// доделать добавление в корзину
+				$this->addProduct($request, $product, $request->get('quantity', 1));
 
-				$products = [];
+				$products = $this->getProducts();
 				
 				return \Response::json([
 					'success' => true,
@@ -47,7 +48,33 @@ class Cart extends BaseController
 	
 	public function removeFromCart(Request $request)
 	{
+		// доделать
 		
 	}
-	
+
+
+	/**
+	 * Add product to cart with session
+	 *
+	 * @param Request $request
+	 * @param $product
+	 * @param int $quantity
+	 * @author     It Hill (it-hill.com@yandex.ua)
+	 * @copyright  Copyright (c) 2015-2016 Website development studio It Hill (http://www.it-hill.com)
+	 */
+	protected function addProduct(Request $request, $product, $quantity = 1)
+	{
+		$cartProducts = $request->session()->get('cart', []);
+		$cartProducts[] = [
+			'product_id' => $product->id,
+			'quantity' => $quantity,
+		];
+		$request->session()->put('cart', $cartProducts);
+	}
+
+
+	protected function getProducts()
+	{
+		return [];
+	}
 }
