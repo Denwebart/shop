8e3fec774a9f76c98467f9765b1d718a161c89ff
Annywebart <annywebart@yandex.ua>
2016-06-30 22:20:49 +0300

    Админка: добавление, удаление, редактирование дополнительных изображений для товара (исправлено).

--- app/Modules/Admin/Views/products/images.blade.php
+++ app/Modules/Admin/Views/products/images.blade.php
@@ -7,12 +7,12 @@
 
 @foreach($product->images as $image)
     <div class="file-box">
-        {!! Form::file('image['. $image->id .']', ['id' => 'image['. $image->id .']', 'class' => 'img-responsive img-thumbnail dropify-more', 'data-default-file' => $image->getImageUrl(), 'data-max-file-size' => '3M', 'data-image-id' => $image->id, 'data-height' => '100']) !!}
+        {!! Form::file('images['. $image->id .']', ['id' => 'images-'. $image->id, 'class' => 'img-responsive img-thumbnail dropify-more', 'data-default-file' => $image->getImageUrl(), 'data-max-file-size' => '3M', 'data-image-id' => $image->id, 'data-height' => '100']) !!}
     </div>
 @endforeach
 
 <div class="file-box new-product-image hidden">
-	{!! Form::file('image[new]', ['id' => 'image[new]', 'class' => 'img-responsive img-thumbnail dropify-more', 'data-default-file' => false, 'data-max-file-size' => '3M', 'data-image-id' => 0, 'data-height' => '100']) !!}
+	{!! Form::file('images[new]', ['id' => 'images-new', 'class' => 'img-responsive img-thumbnail dropify-more', 'data-default-file' => false, 'data-max-file-size' => '3M', 'data-image-id' => 0, 'data-height' => '100']) !!}
 </div>
 
 <div class="file-box m-l-15">
