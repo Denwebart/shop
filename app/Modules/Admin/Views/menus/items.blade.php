7672ed24b72d6ee092c3ebb8e2825d626d9a3254
Annywebart <annywebart@yandex.ua>
2016-07-02 04:16:05 +0300

    Админка: меню в настройках (стили).

--- app/Modules/Admin/Views/menus/items.blade.php
+++ app/Modules/Admin/Views/menus/items.blade.php
@@ -5,9 +5,18 @@
  */
 ?>
 
-<ul class="form-editable sortable todo">
+<ul class="form-editable sortable todo list-group user-list">
     @foreach($items as $item)
-        <li id="{{ $item->id }}" data-item-id="{{ $item->id }}" data-page-id="{{ $item->page->id }}" data-menu-type="{{ $menuType }}">
+        <li class="list-group-item" id="{{ $item->id }}" data-item-id="{{ $item->id }}" data-page-id="{{ $item->page->id }}" data-menu-type="{{ $menuType }}">
+            <span class="icon pull-left m-r-10">
+                @if($item->page->type == \App\Models\Page::TYPE_PAGE && $item->page->is_container)
+                    <i class="fa fa-folder" title="Категория" data-toggle="tooltip"></i>
+                @elseif($item->page->type == \App\Models\Page::TYPE_CATALOG)
+                    <i class="fa fa-shopping-bag" title="Каталог товаров" data-toggle="tooltip"></i>
+                @else
+                    <i class="fa fa-file-o" title="Страница" data-toggle="tooltip"></i>
+                @endif
+            </span>
             <span class="title pull-left">
                 <a href="#" class="editable-menu-item" data-value="{{ $item->page->getTitle() }}" data-type="text" data-pk="{{ $item->id }}" data-page-id="{{ $item->page->id }}">{{ $item->page->getTitle() }}</a>
             </span>
@@ -18,7 +27,7 @@
             @if(count($item->children))
                 <ul class="sortable-sublist sublist margin-top-10">
                     @foreach($item->children as $itemChild)
-                        <li id="{{ $itemChild->id }}" data-item-id="{{ $itemChild->id }}" data-page-id="{{ $itemChild->page->id }}" data-menu-type="{{ $menuType }}">
+                        <li class="list-group-item" id="{{ $itemChild->id }}" data-item-id="{{ $itemChild->id }}" data-page-id="{{ $itemChild->page->id }}" data-menu-type="{{ $menuType }}">
                             <span class="title pull-left">
                                 <a href="#" class="editable-menu-item" data-value="{{ $itemChild->page->getTitle() }}" data-type="text" data-pk="{{ $itemChild->id }}" data-page-id="{{ $itemChild->page->id }}">{{ $itemChild->page->getTitle() }}</a>
                             </span>
