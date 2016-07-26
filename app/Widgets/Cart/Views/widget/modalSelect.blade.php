104d69bedc98062f04856c6523c04c45f0cf204d
Annywebart <annywebart@yandex.ua>
2016-07-26 04:36:21 +0300

    Оптимизация (ускорение) сайта.

--- app/Widgets/Cart/Views/widget/modalSelect.blade.php
+++ app/Widgets/Cart/Views/widget/modalSelect.blade.php
@@ -12,20 +12,21 @@
 
     <div class="col-md-7 col-sm-7 col-xs-7 align-left">
 
-        <h4>Выберите обязательные характеристики</h4>
+        <h4 class="m-t-10">Добавление товара в корзину</h4>
 
         @include('parts.productPropertySize', ['product' => $product])
 
         @include('parts.productPropertyColor', ['product' => $product])
-    </div>
 
-    <div class="col-md-12 col-sm-12 col-xs-12">
-        <button class="btn btn--wd buy-link add-to-cart" data-product-id="{{ $product->id }}">
-            <span class="icon icon-ecommerce"></span>
-            <span class="product-preview__info__link__text text-uppercase">
-                <span class="hidden-md hidden-sm hidden-xs">Добавить в корзину</span>
-                <span class="hidden-lg">B корзину</span>
-            </span>
-        </button>
+        <div class="divider divider--xs"></div>
+        <div class="align-center">
+            <button class="btn btn--wd buy-link add-to-cart" data-product-id="{{ $product->id }}">
+                <span class="icon icon-ecommerce"></span>
+                <span class="product-preview__info__link__text text-uppercase">
+                    <span class="hidden-md hidden-sm hidden-xs">Добавить в корзину</span>
+                    <span class="hidden-lg">B корзину</span>
+                </span>
+            </button>
+        </div>
     </div>
 </div>
