73003dd3c22b8fa540af2f47f780fea9cb377cce
Annywebart <annywebart@yandex.ua>
2016-07-27 17:51:02 +0300

    Исправление ошибки при выборе цвета на товаре (если цвет выбран в url - заполнять как выбранный).

--- resources/views/parts/productPropertyColor.blade.php
+++ resources/views/parts/productPropertyColor.blade.php
@@ -9,12 +9,13 @@
     <label>Цвет:</label>
     <ul class="options-swatch options-swatch--color options-swatch--lg">
         @foreach($product->propertyColor as $color)
-            <li class="add-to-cart__color tooltip-link @if(Request::get($color->property_title) == $color->value || (isset($options['color']) && $options['color'] == $color->value) || count($product->propertyColor) == 1) active @endif" title="{{ $color->value }}" data-value="{{ $color->value }}">
+            @php($colorPropertyTitle = $color->property ? $color->property->title : null)
+            <li class="add-to-cart__color tooltip-link @if(Request::get($colorPropertyTitle) == $color->value || (isset($options['color']) && $options['color'] == $color->value) || count($product->propertyColor) == 1) active @endif" title="{{ $color->value }}" data-value="{{ $color->value }}">
                 <span class="swatch-label color-icon color" style="background: {{ $color->additional_value or '#ffffff' }}"></span>
             </li>
         @endforeach
     </ul>
     <span class="help-block error add-to-cart__color___error"></span>
-    {!! Form::hidden('add-to-cart__color__input', Request::get($color->property_title, isset($options['color']) ? $options['color'] : '')) !!}
+    {!! Form::hidden('add-to-cart__color__input', Request::get($colorPropertyTitle, isset($options['color']) ? $options['color'] : '')) !!}
     <div class="divider divider--sm"></div>
 @endif
--- resources/views/parts/productPropertySize.blade.php
+++ resources/views/parts/productPropertySize.blade.php
@@ -9,12 +9,13 @@
     <label>Размер:</label>
     <ul class="options-swatch options-swatch--size options-swatch--lg">
         @foreach($product->propertySize as $size)
-        <li class="add-to-cart__size tooltip-link @if(Request::get($size->property_title) == $size->value || (isset($options['size']) && $options['size'] == $size->value) || count($product->propertySize) == 1) active @endif" title="{{ $size->value }} @if($size->additional_value) ({{ $size->additional_value }}) @endif" data-value="{{ $size->value }}">
-            <span class="swatch-label">{{ $size->value }}</span>
-        </li>
+            @php($sizePropertyTitle = $size->property ? $size->property->title : null)
+            <li class="add-to-cart__size tooltip-link @if(Request::get($sizePropertyTitle) == $size->value || (isset($options['size']) && $options['size'] == $size->value) || count($product->propertySize) == 1) active @endif" title="{{ $size->value }} @if($size->additional_value) ({{ $size->additional_value }}) @endif" data-value="{{ $size->value }}">
+                <span class="swatch-label">{{ $size->value }}</span>
+            </li>
         @endforeach
     </ul>
     <span class="help-block error add-to-cart__size___error"></span>
-    {!! Form::hidden('add-to-cart__size__input', Request::get($size->property_title, isset($options['size']) ? $options['size'] : '')) !!}
+    {!! Form::hidden('add-to-cart__size__input', Request::get($sizePropertyTitle, isset($options['size']) ? $options['size'] : '')) !!}
     <div class="divider divider--xs"></div>
 @endif
